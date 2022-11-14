cb3b454292e2e7afd17bbe1fbb043a3e96f3f997
adj5672 <adj5672@naver.com>
2022-11-14 10:48:50 +0900

    DataErrorResponse 추가

---
+++ src/main/java/io/github/adj5672/enumerateexception/response/DataErrorResponse.java
@@ -0,0 +1,18 @@
+package io.github.adj5672.enumerateexception.response;
+
+public class DataErrorResponse extends BaseErrorResponse {
+
+    private Object data;
+
+    public DataErrorResponse() {
+    }
+
+    public DataErrorResponse(int code, String message, Object data) {
+        super(code, message);
+        this.data = data;
+    }
+
+    public Object getData() {
+        return this.data;
+    }
+}
